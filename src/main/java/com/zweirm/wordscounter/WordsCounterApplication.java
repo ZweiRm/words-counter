1c44f1b1fd9efa9f90ee01671d4d9a432e4a608e
ZweiRm <momg@foxmail.com>
2018-03-01 16:05:40 +0800

    create project

---
+++ src/main/java/com/zweirm/wordscounter/WordsCounterApplication.java
@@ -0,0 +1,12 @@
+package com.zweirm.wordscounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordsCounterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zweirm/wordscounter/WordsCounterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zweirm.wordscounter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WordsCounterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
